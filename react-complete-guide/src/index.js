31767402d9a3da50d43635ade812d4e1f8fa7613
adl.wisnup <adl.wisnup@xl.co.id>
2022-04-30 00:54:50 +0700

    started section 6

--- react-complete-guide/src/index.js
+++ react-complete-guide/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./section-5/App";
+import App from "./section-6/App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
