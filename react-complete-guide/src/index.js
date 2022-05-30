8ca27eea97ccec889c656368c6e0aa58bf49e2ab
wisnuprsj <wisnuprsj@gmail.com>
2022-05-30 13:47:29 +0700

    finished section 13

--- react-complete-guide/src/index.js
+++ react-complete-guide/src/index.js
@@ -1,17 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./section-12/App";
+import App from "./section-13/App";
 import reportWebVitals from "./reportWebVitals";
 import { AuthContextProvider } from "./section-10/store/auth-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
-  </React.StrictMode>
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
