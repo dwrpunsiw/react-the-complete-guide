284635727c454d495f382c7b69af30bd47123109
wisnuprsj <wisnuprsj@gmail.com>
2022-05-30 15:36:23 +0700

    finished section 14

--- react-complete-guide/src/index.js
+++ react-complete-guide/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./section-13/App";
+import App from "./section-14/App";
 import reportWebVitals from "./reportWebVitals";
 import { AuthContextProvider } from "./section-10/store/auth-context";
 
