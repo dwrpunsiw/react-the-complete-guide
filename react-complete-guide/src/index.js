f944acf1c3fd9b5aa9b3ca6d2fd5db565c5bbc21
wisnuprsj <wisnuprsj@gmail.com>
2022-08-18 13:40:58 +0700

    finished section 18

--- react-complete-guide/src/index.js
+++ react-complete-guide/src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./section-17/App";
+import App from "./section-18/App";
 import reportWebVitals from "./reportWebVitals";
 import { AuthContextProvider } from "./section-10/store/auth-context";
+import { Provider } from "react-redux";
+import { store } from "./section-18/store/index";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthContextProvider>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </AuthContextProvider>
 );
 
